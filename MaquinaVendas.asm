69240720fca4ce80a273ddd547f4ab4156098631
KingRichardpt45 <ricfrancvi@gmail.com>
2023-04-10 23:36:31 +0100

    inacabado

--- MaquinaVendas.asm
+++ MaquinaVendas.asm
@@ -393,7 +393,9 @@
 	;--------------------------------------------------------------------------------------------------------------------------------
 		
 		OPTN_Y 						EQU 24C5H ;2480H + 70 (posisao do max)
-		B1_Linha1_Stock				EQU 2810H ;2800H + 16 (primeiro da segunda linha)
+		Byte1_Linha1_Stock				EQU 2810H ;2800H + 16 (primeiro da segunda linha)
+		Stock_X				EQU ---- ;2800H + 16 (primeiro da segunda linha)
+		Stock_Y				EQU ---- ;2800H + 16 (primeiro da segunda linha)
 	;--------------------------------------------------------------------------------------------------------------------------------
 	;													Constantes
 	;--------------------------------------------------------------------------------------------------------------------------------
@@ -475,6 +477,7 @@ Mostrar_Stock:
 	PUSH R1							; guarda o valor atual de R1
 	MOV R1 , ARG1					; R1 tem agora o valor de do endereco da variavel que guarda a pagina a mostrar
 	MOV [R1] , Display_Stock 		; ARG1 ficao com o endereco da pagina Display_Stock
+;	Call CompletarPagina_Stock		; completa a pagina
 	CALLF Mostrar_Display    		;
 	CALLF LerInput_OPTN				;
 	MOV R1, PER_EN_VALOR			; R1 passa a ter o valor do endereco do PER_EN_VALOR
@@ -557,15 +560,119 @@ ComparaPass:
 ;											Rotinas do Display dinamico 
 ;--------------------------------------------------------------------------------------------------------------------------------
 
-;Rotina para completar a pagina Display_stock  ARG1 = Primeiro elemento a ser escrito ARG2 = ultimo endereco
-Completar_Pagina:
-	PUSH R0
-	PUSH R1
-	PUSH R2
+;Rotina para completar a pagina Display_stock  ARG1 = Primeiro endereco elemento a ser escrito ARG2 = ultimo endereco ARG3 tamanho reust ARG1 = Numero de linhas escritas
+CompletarPagina_Stock:
+	PUSH R0							; guarda o valor de R0
+	PUSH R1							; guarda o valor de R1
+	PUSH R2							; ...
+	PUSH R3
+	PUSH R4
+	PUSH R5
+	PUSH R6
+	PUSH R7
+	PUSH R8
+	PUSH R9							; guarda o valor de R9
 	MOV R1 , ARG1 					; R1 fica com o endereco do ARG1
-	MOV R0 , [R1]					; R0 fica com o valor do ARG1 
-	\
+	MOV R0 , [R1]					; R0 fica com o valor do ARG1
+	MOV R2 , ARG2					; R2 fica com o endereco do ARG2
+	MOV R1 , [R2]					; R1 fica com o valor do ARG2
+	MOV R3 , ARG3					; R3 fica com o endereco do ARG3
+	MOV R4 , R1			
+	SUB R4 , R0
+	MOV [R2] , R4					; ARG2 fica com o tamanho (o ultimo endereco - o primeiro)
+;escrita do numero da pg	
+	CALLF ContarPaginas				; call contar paginas
+	MOV R4 , [R1]					; R4 fica com o numero da pagina atual
+	MOV R5 , 10						; R5 = 10
+	CMP R4 , R5						; compare se o numero de pagina é maior que 10
+	MOV R5 , Stock_X				; R5 fica com o endereco do x do Display stock
+	JLE CompletarPagina_Stock_CMP1_fim	; se for R4 < 10
+	MOV R6 , 48						; numero para converter um numero em char
+	MOV R7 , R4						; copiar o valor de R4
+	MOV R8 , 10						; R8 = 10
+	MOD R7 , R8						; ex: 92 Mod 10 = 2
+	MOV R9 , R7						; R9 tem as unidades do numer R4 ex: R9 = 2
+	ADD R7 , R6						; Passar R7 para Char numero
+	MOVB [R5] , R7					; escrever unidadess
+	MOV R7 , R4						; copiar o valor de R4
+	SUB R7 , R9						; sub a R7 as unidades
+	MOV R8 , 10						; 90 / 10 = 9
+	DIV R7 , R8						; R7 tem as desenas R7 = 9
+	ADD R7 , R6						; Passar R7 para Char numero
+	SUB R5 , 1						; posisao anterior as unidades
+	MOVB [R5] , R7					; escrever desenas
+CompletarPagina_Stock_CMP1_fim:
+	MOV [R5] , R4					; escreve no lugar de X o valor de R4
+	MOV R4 , [R2]					; R4 fica com o numero do max de paginas
+	MOV R5 , 10						; R5 = 10
+	CMP R4 , R5						; compare se o numero de pagina é maior que 10
+	MOV R5 , Stock_Y				; R5 fica com o endereco do y do Display stock
+	JLE CompletarPagina_Stock_CMP1_fim	; se for R4 < 10
+	ADD R5 , 1						; R5 fica com posisao das unidades
+	MOV R6 , 48						; numero para converter um numero em char
+	MOV R7 , R4						; copiar o valor de R4
+	MOV R8 , 10						; R8 = 10
+	MOD R7 , R8						; ex: 92 Mod 10 = 2
+	MOV R9 , R7						; R9 tem as unidades do numer R4 ex: R9 = 2
+	ADD R7 , R6						; Passar R7 para Char numero
+	MOVB [R5] , R7					; escrever unidadess
+	MOV R7 , R4						; copiar o valor de R4
+	SUB R7 , R9						; sub a R7 as unidades
+	MOV R8 , 10						; 90 / 10 = 9
+	DIV R7 , R8						; R7 tem as desenas R7 = 9
+	ADD R7 , R6						; Passar R7 para Char numero
+	SUB R5 , 1						; posisao anterior as unidades
+	MOVB [R5] , R7					; escrever desenas
+CompletarPagina_Stock_CMP2_fim:
+	MOVB [R5] , R4					; escrever o max de paginas
+;escrita do numero da pg fim
+	MOV R4 , Byte1_Linha1_Stock		; R4 fica com o valor do 1 byte da 1 linha vazia do stock 
+	MOV R5 , Size_Stockitem			; R3 fica com o tamanho do item
+CompletarPagina_Ciclo:
+	CMP R1 , R2						; compara o R0 (primeiro elemento / atual) com R1 (ultimo elemento)
+	JGT	CompletarPagina_FimCiclo	; se o atual maior que o ultimo termina
+	CallF CompletarPagina_Ciclo		; completa a linha
+	ADD R0 , R3						; avanca R0 para o item seguint 
+	JMP Completar_PaginaCiclo		; volta a completar mais uma linha
+CompletarPagina_FimCiclo:
 
+
+;rotina usada para escrever o nome e ea quantidade numa linha
+Completar_Linha_Stock:
+	
+
+;Rotina para completar a pagina Display_stock  ARG1 = Primeiro elemento a ser escrito ARG2 tamanho resultado pagina atual fica ARG1 e max PAGES fica no ARG2
+ContarPaginas:
+	PUSH R0							; guarda o valor atual de R0
+	PUSH R1 						; guarda o valor atual de R1
+	PUSH R2							; guarda o valor atual de R2
+	PUSH R3							; guarda o valor atual de R3
+	PUSH R4							; guarda o valor atual de R4
+	PUSH R5							; guarda o valor atual de R5
+	MOV R1 , ARG1 					; R1 fica com o endereco do ARG1
+	MOV R0 , [R1]					; R0 fica com o valor do ARG1
+	MOV R2 , ARG2					; R2 fica com o endereco do ARG2
+	MOV R3 , [R2]					; R3 fica com o valor do ARG2 = tamanho
+	MOV R4 , 0						; R4 fica com 0
+	MOV R5 , 0						; R5 fica com 0
+ContarPaginas_Ciclo:
+	ADD R4 , 4						; incrementa o contador R4 em 4 (numero max de linhas)
+	ADD R5 , 1						; incrementa o contador R5 em 1 (mais uma pagina)
+	CMP R0 , R4						; compara o primeiro elemento R0 com o maior elemento posivel atual R4
+	JGE ContarPaginas_CMP2			; se R4 mair que o primerio elemento 
+	MOV [R1] , R5					; ARG1 fica com o resultado do contador R5 
+ContarPaginas_CMP2:
+	CMP R3 , R4						; compara se o maior elemento posivel atual R4 é maior que o tamanho
+	JLE ContarPaginas_Ciclo			; se o contador R4 for maior ou igual que o tamanho nao  ha mais paginas 
+	MOV [R2] , R5					; ARG2 fica com o resultado do contador R5 (numero de paginas)
+	POP R5							; guarda o valor atual de R5
+	POP R4 							; guarda o valor atual de R4
+	POP R3							; guarda o valor atual de R3
+	POP R2							; guarda o valor atual de R2
+	POP R1							; guarda o valor atual de R1
+	POP R0							; guarda o valor atual de R0
+	RETF
+	
 ;Rotina para mosstrar a pagina de erro Opcao invalida ARG1 = MAX OPTN (que no max e 7)
 Mostrar_ErrorDisplay_OPTN:
 	PUSH R0							; guarda o valor atual de R0
